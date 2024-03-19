4575b3c8e1be49b85543e6349e40f35ded0cdb89
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-03-19 03:29:37 +0200

    1. Generated user service and model. 2. Created users table. 3. Handled retrieving data from DB. 4. Handled removing a single user from DB. 5. Only the admins can retrieve/view all users and delete them. 6. Modifed the app.routes to use HttpClient to fix it's errors and provided the UserService in it.

--- src/app/components/users-parent/users-parent.component.ts
+++ src/app/components/users-parent/users-parent.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IUser } from '../../models/iuser';
+import { UsersService } from '../../services/users.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-users-parent',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './users-parent.component.html',
-  styleUrl: './users-parent.component.scss'
+  styleUrls: ['./users-parent.component.scss'],
 })
-export class UsersParentComponent {
+export class UsersParentComponent implements OnInit, OnDestroy {
+  users: IUser[] | undefined;
+  usersRequest: any;
+  constructor(private UserService: UsersService) {}
 
+  deleteUser(userId: string) {
+    if (confirm('Are you sure you want to delete this user') === true) {
+      this.UserService.deleteUser(userId).subscribe((res) => {
+        console.log(res);
+      });
+      location.reload();
+      return;
+    } else {
+      alert('User not deleted');
+    }
+  }
+  ngOnInit(): void {
+    const adminToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdDIiLCJpZCI6IjY1ZjU3YjNiNGEwOTcxYjA1YjE5Y2MzYSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcxMDgwOTM0NiwiZXhwIjoxNzEwODIzNzQ2fQ.Nvo0dTDU4VtvwVnGg94MHV1GDrFEVbaj1iTfohABxDM';
+    this.UserService.setAdminToken(adminToken);
+    this.usersRequest = this.UserService.getUsers().subscribe((users) => {
+      this.users = users.data;
+      console.log(this.users);
+    });
+  }
+  ngOnDestroy(): void {
+    this.usersRequest.unsubscribe();
+  }
 }
