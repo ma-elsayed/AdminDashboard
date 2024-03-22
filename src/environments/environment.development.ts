7cd7c52ea65a91fb59abcfe03fd0d70e1c660f59
Mostafa_Ali <mostafaqaraman@outlook.com>
2024-03-22 17:31:41 +0200

    fixed header and sidebar with the router-outlet

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   baseURL: 'http://localhost:3090',
   adminToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdHJvbGUiLCJpZCI6IjY1ZjkyNzk2NzdkYzM5MzEzYmVhMGY1MyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcxMTA3MDI5OSwiZXhwIjoxNzExMDg0Njk5fQ.7JgF_ZQaMoDd-Ig6oeu6ds7hQyDik3cXj4veQB5j2aU',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdHJvbGUiLCJpZCI6IjY1ZjkyNzk2NzdkYzM5MzEzYmVhMGY1MyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcxMTExNTM0MywiZXhwIjoxNzExMTI5NzQzfQ.fyhtxKmjksgRUJho3ONALJzZBZk09q2FHK0S8wu6w4Y',
 };
